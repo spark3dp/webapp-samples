2fb47eb41456d3fa5d69b89a4cc91f79ba51046c
telaviv1 <telaviv1@mail.com>
2014-12-02 18:19:13 +0200

    Update login.js

--- scripts/login.js
+++ scripts/login.js
@@ -4,14 +4,15 @@ $(function ($) {
 
     //New page load and no token yet
     if (!code && !localStorage.getItem('spark-drive-token')) {
+        //Get the Auth Code which is a required when requesting an access token.
         sparkDrive.redirectToAuthLoginURL();
-    //Came from redirection or we have token in local storage
+    //Either returning from getting the Auth Code or there is an access token already in storage
     } else {
         sparkDrive.getAccessToken(code, function(token){
             $('body').removeClass('hidden');
             if (token) {
                 console.log(token);
-
+    // replace "/drive-gallery" with the path of your web server folder whcih contains the sample code
                 window.location = window.location.protocol + "//" + window.location.hostname + '/drive-gallery';
             } else {
                 console.log('Problem with fetching token');
