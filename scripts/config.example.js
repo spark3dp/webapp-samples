a351244ef93814e2f0d05b4816bf6a088384e66d
Greg Rashkevitch <greg.rashkevitch@autodesk.com>
2015-02-12 18:18:26 +0200

    Update config.example.js

--- scripts/config.example.js
+++ scripts/config.example.js
@@ -1,5 +1,5 @@
 var CLIENT_ID = '<enter your APP key here>';
 var AUTH_HASH = '<Base64(app key + ":" + app secret)>'; // Base64 encrypted app key + : + app secret
-var REDIRECT_URL = '<address of your config.html file which is the same as your app\'s redirect url>';// The redirect URI you've set for your application
+var REDIRECT_URL = '<address of your callback.html file which is the same as your app\'s redirect url>';// The redirect URI you've set for your application
 var APP_HOME = '<address of your app\'s home>';// The home page for your application
 var ENVIRONMENT = 'sandbox'; // sandbox/production
