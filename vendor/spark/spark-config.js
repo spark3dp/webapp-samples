ebae0e9849b69c4dcf9cd0d9084474a0d421fa53
Gregory Rashkevitch <greg.rashkevitch@autodesk.com>
2015-02-16 09:38:28 +0200

    Change all calls to https

--- vendor/spark/spark-config.js
+++ vendor/spark/spark-config.js
@@ -4,5 +4,5 @@ var apiHostSandbox = 'sandbox.spark.autodesk.com/api/v1',
 	apiHostProduction = 'api.spark.autodesk.com/api/v1',
 	apiHost = ENVIRONMENT === 'production' ? apiHostProduction : apiHostSandbox,
 	endUserAuthorizationEndpoint = apiHost + "/oauth/authorize", // The page of the authorize
-	protocol = ENVIRONMENT === 'production' ? 'https' : 'http' //The protocol of the server
+	protocol = 'https'; //The protocol of the server
 ;
