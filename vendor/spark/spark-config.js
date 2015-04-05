dbeb84a68cb023113debda0718df8afe9084de1f
Michael Rozman <michael.rozman@autodesk.com>
2015-04-05 11:43:28 +0300

    changed sandbox server to alpha

--- vendor/spark/spark-config.js
+++ vendor/spark/spark-config.js
@@ -1,6 +1,7 @@
 //Various Spark config parameters
 
-var apiHostSandbox = 'sandbox.spark.autodesk.com/api/v1',
+//var apiHostSandbox = 'sandbox.spark.autodesk.com/api/v1',
+var apiHostSandbox = 'api-alpha.spark.autodesk.com/api/v1',
 	apiHostProduction = 'api.spark.autodesk.com/api/v1',
 	apiHost = ENVIRONMENT === 'production' ? apiHostProduction : apiHostSandbox,
 	endUserAuthorizationEndpoint = apiHost + "/oauth/authorize", // The page of the authorize
