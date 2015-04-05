b9fc84b6a968e1ea0b146bcae8d3e9f9e8878804
Gregory Rashkevitch <greg.rashkevitch@autodesk.com>
2015-04-05 17:26:05 +0300

    add auth through server

---
+++ server/nodejs/config.exampe.js
@@ -0,0 +1,6 @@
+module.exports = {
+	CLIENT_ID: '<enter your APP key here>',
+	CLIENT_SECRET:'<enter your APP secret here>',
+	CALLBACK_URI:'<address of your callback url which is the same as your app\'s redirect url>',// The redirect URI you've set for your application
+	HOME_URI:'<address of your app\'s home>'// The home page for your application
+};
