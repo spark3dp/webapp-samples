ad89522bbdab58a26fb427855258807cb4707633
Michael Rozman <michael.rozman@autodesk.com>
2015-04-12 17:03:00 +0300

    added comments to print sdk

--- vendor/spark/spark-printer.js
+++ vendor/spark/spark-printer.js
@@ -14,7 +14,13 @@ var sparkPrint = function () {
 	return {
 
 
-		registerPrinter: function (token,printerName,callback,errorCallback) {
+		/**
+		 * Registers a printer for the logged in user.
+		 * @param printerName
+		 * @param callback
+		 * @param errorCallback
+		 */
+		registerPrinter: function (token, printerName,callback,errorCallback) {
 			//Make sure token is still valid
 			if (sparkAuth.isTokenValid()) {
 
@@ -32,25 +38,38 @@ var sparkPrint = function () {
 		},
 
 
-		getAllPrinters: function (callback) {
+		/**
+		 * Gets all printer that are registered to the logged in user
+		 * @param callback
+		 * @param errorCallback
+		 */
+		getAllPrinters: function (callback,errorCallback) {
 			//Make sure token is still valid
 			if (sparkAuth.isTokenValid()) {
 				var headers = {
 					"Authorization": "Bearer " + sparkAuth.accessToken(),
 					"Content-type": "application/x-www-form-urlencoded"
 				};
-				Util.xhr(serverUrl + 'print/printers', 'GET', '', headers, callback);
+				Util.xhr(serverUrl + 'print/printers', 'GET', '', headers, callback,errorCallback);
 			}else{
 				callback(false);
 			}
 
 		},
 
-		printJob: function (fileUrl, printerId,settings,callback,errorCallback) {
+		/**
+		 * Sends a print job
+		 * @param printableId
+		 * @param printerId
+		 * @param settings
+		 * @param callback
+		 * @param errorCallback
+		 */
+		printJob: function (printableId, printerId,settings,callback,errorCallback) {
 			//Make sure token is still valid
 			if (sparkAuth.isTokenValid()) {
 
-				var params =JSON.stringify( {printable_id:fileUrl,settings:settings});
+				var params =JSON.stringify( {printable_id:printableId,settings:settings});
 				//var params = {printable_id:fileUrl,settings:settings};
 				console.log(params);
 				var headers = {
@@ -66,30 +85,40 @@ var sparkPrint = function () {
 		},
 
 		/**
-		 * Create a new asset
-		 * @param assetPost
+		 * Returns all job statuses for printer specified by printerId
+		 * @param printerId
+		 * @param callback
+		 * @param errorCallback
 		 */
-		getJobsStatusByPrinter: function (printerId,callback) {
+		getJobsStatusByPrinter: function (printerId,callback,errorCallback) {
 			//Make sure token is still valid
 			if (sparkAuth.isTokenValid()) {
 				var headers = {
 					"Authorization": "Bearer " + sparkAuth.accessToken(),
 					"Content-type": "application/x-www-form-urlencoded"
 				};
-				Util.xhr(serverUrl + 'print/printers/' + printerId + "/jobs", 'GET', '', headers, callback);
+				Util.xhr(serverUrl + 'print/printers/' + printerId + "/jobs", 'GET', '', headers, callback,errorCallback);
 			}else{
 				callback(false);
 			}
 
 		},
 
+		/**
+		 * Sends a command(resume/pause/cancel) to a specific printer with specific job
+		 * @param printerID
+		 * @param jobId
+		 * @param command - resume/pause/cancel
+		 * @param callback
+		 * @param errorCallback
+		 */
 		sendPrintCommand: function(printerID,jobId,command,callback,errorCallback){
 			if (sparkAuth.isTokenValid()) {
 				var headers = {
 					"Authorization": "Bearer " + sparkAuth.accessToken(),
 					"Content-type": "application/x-www-form-urlencoded"
 				};
-				Util.xhr(serverUrl + 'print/printers/' + printerID + "/command", 'POST', 'command='+command+"&job_id="+jobId, headers, callback);
+				Util.xhr(serverUrl + 'print/printers/' + printerID + "/command", 'POST', 'command='+command+"&job_id="+jobId, headers, callback,errorCallback);
 			}else{
 				callback(false);
 			}
