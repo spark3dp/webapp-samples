0f4346a86fdb793717db9d2065450f74907dda44
Michael Rozman <michael.rozman@autodesk.com>
2015-04-20 17:43:49 +0300

    applied new table layout to manage-printers, new login

--- plugins/login/login.js
+++ plugins/login/login.js
@@ -19,7 +19,13 @@ jQuery(function ($) {
 		$('.logged-in-container').hide();
 
 		var div = $('<div id="auth-iframe-wrapper"><iframe style="width: 100%;border: 0;height: 500px;"></iframe></div>');
-		$("body").append(div);
+		var loginContainer = $(".login-container");
+		if(loginContainer.length>0){
+			loginContainer.append(div);
+		}
+		else {
+			$("body").append(div);
+		}
 		$('#auth-iframe-wrapper iframe').attr('src', spark.auth.getAuthLoginUrl());
 		$('.logged-in-container').hide();
 	}
