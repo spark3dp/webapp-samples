24b106769baf0a00ffeffae505defa64056d988a
Einat Bertenthal <einat.bertenthal@autodesk.com>
2015-04-29 18:35:44 +0300

    menu

--- plugins/state/statePlugin.js
+++ plugins/state/statePlugin.js
@@ -13,12 +13,28 @@ var statePlugin = function($){
 		else{
 			$(tabSelector).removeClass("disabled");
 		}
+
+		$(tabSelector).removeClass("selected");
+	};
+
+	var setTabVerified = function(tab){
+
+		var enabledTabs = $(tab).siblings().parents().first().children().not(".disabled");
+
+		if(enabledTabs.length > 1){
+			enabledTabs.each(function(index, element){
+				if (index < enabledTabs.length - 1) {
+					$(this).addClass("verified");
+				}
+			});
+		}
 	};
 
 	var changeState = function(stateName,optionalParams){
 		if(states[stateName]!=undefined){
 
 			_currentState = stateName;
+			var selectedTab = states[stateName].selectedTab;
 			var enabledTabs = states[stateName].enabledTabs;
 			var disabledTabs = states[stateName].disabledTabs;
 
@@ -33,6 +49,12 @@ var statePlugin = function($){
 			for(var i in disabledTabs){
 				setTabEnable(disabledTabs[i],false);
 			}
+
+			if(enabledTabs.length > 0) {
+				setTabVerified(enabledTabs[0]);
+			}
+			$(selectedTab).addClass("selected");
+
 			$("#mainIframe").attr("src",srcIframe);
 		}
 	};
