d565a97608fb718a4edad3c3de2a14368f1cdde1
Michael Rozman <michael.rozman@autodesk.com>
2015-07-07 11:35:59 +0300

    fix sample-app for changes in authentication

--- assets/scripts/init.js
+++ assets/scripts/init.js
@@ -3,14 +3,15 @@ var APP_KEY = APP_KEY || '',
 	GUEST_TOKEN_URL = GUEST_TOKEN_URL || 'http://localhost:3000/guest_token',
 	ACCESS_TOKEN_URL = ACCESS_TOKEN_URL || 'http://localhost:3000/access_token',
 	REFRESH_TOKEN_URL = REFRESH_TOKEN_URL || 'http://localhost:3000/refresh_token',
-	API_URL = (typeof ENVIRONMENT !== 'undefined' && ENVIRONMENT === 'production') ? ADSKSpark.Constants.API_HOST_PROD :
-		ADSKSpark.Constants.API_HOST_SANDBOX,
 	REDIRECT_URI = REDIRECT_URI || '';
 
+var isProd = (ENVIRONMENT == "sandbox") ? false : true;
+
 ADSKSpark.Client.initialize(APP_KEY,// Your app key
+	isProd,
+	REDIRECT_URI,
 	GUEST_TOKEN_URL,// The guest token endpoint that is implemented by your server (i.e. http://example.com/guest_token)
 	ACCESS_TOKEN_URL,// The access token endpoint that is implemented by your server (i.e. http://example.com/access_token)
-	REFRESH_TOKEN_URL,// The refresh access token endpoint that is implemented by your server (i.e. http://example.com/refresh_token)
-	API_URL, // api host
-	REDIRECT_URI
+	REFRESH_TOKEN_URL// The refresh access token endpoint that is implemented by your server (i.e. http://example.com/refresh_token)
+
 );
