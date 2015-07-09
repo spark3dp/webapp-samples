c072007fc68b0cfd04358da88438646f992f1849
Greg Rashkevitch <rashkevitch@gmail.com>
2015-07-09 16:41:08 +0300

    Store refresh token exclusively on server

--- server/nodejs/server.js
+++ server/nodejs/server.js
@@ -1,35 +1,46 @@
-//provide environment through console i.e. ENV=beta node server.js
+// Provide environment through console i.e. ENV=beta node server.js
 var env = process.env.ENV || 'local',
 	port = process.env.PORT || 3000,
-	config = require('./config.js'),
-	API_SERVER = (env === 'prod' ? 'api.spark.autodesk.com/api/v1' : 'https://sandbox.spark.autodesk.com/api/v1');
+	API_SERVER = (env === 'prod' ? 'https://api.spark.autodesk.com/api/v1' : 'https://sandbox.spark.autodesk.com/api/v1'),
+	config = require('./config.js');
 
-
+//Make sure port is a number
 if (isNaN(port)){
 	port = 3000;
 }
 
-//setup express + request
+// Setup dependencies
 var express = require('express'),
 	app = express(),
-	request = require('request');
+	request = require('request'),
+	session = require('express-session'),
+	cookieParser = require('cookie-parser');
 
-
+app.use(cookieParser());
+app.use(session({
+		cookie: {maxAge: 720000},
+		resave: false,
+		saveUninitialized: false,
+		secret: 'spark secret string'
+	})
+);
 
 /**
- * Encode string in base64
- * @param str
- * @returns {*}
+ * Encode a string in base64.
+ *
+ * @param {String} s - The string to encode.
+ * @returns {String} - The base64 encoding of s.
  */
-var toBase64 = function(str){
-	return new Buffer(str).toString('base64');
-}
+var toBase64 = function(s){
+	return new Buffer(s).toString('base64');
+};
 
-
-//Enable CORS
+// Enable CORS
 app.use(function(req, res, next) {
-	res.header("Access-Control-Allow-Origin", "*");
-	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	res.header('Access-Control-Allow-Credentials', true);
+	res.header('Access-Control-Allow-Origin', req.headers.origin);
+	res.header('Access-Control-Allow-Methods', 'OPTIONS,GET,PUT,POST,DELETE');
+	res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Authorization, Content-Type, Accept');
 	next();
 });
 
@@ -61,12 +72,15 @@ app.get('/access_token', function(req, res){
 		body: params,
 		method: 'POST'
 	}, function (err, result, body) {
+		//return the access token object (json)
+		var resp = JSON.parse(body);
 
-		//return the guest token object (json)
-		res.send(body);
+		if (resp.refresh_token) {
+			req.session.refresh_token = JSON.parse(JSON.stringify(resp.refresh_token));
+		}
+
+		res.send(resp);
 	});
-
-
 });
 
 // Guest token service
@@ -75,7 +89,7 @@ app.get('/guest_token', function(req, res){
 	var url = API_SERVER + '/oauth/accesstoken',
 		params = "grant_type=client_credentials",
 		contentLength = params.length,
-	 	headers = {
+		headers = {
 			'Authorization': 'Basic ' + toBase64(config.APP_KEY + ':' + config.APP_SECRET),
 			'Content-Type' : 'application/x-www-form-urlencoded',
 			'Content-Length': contentLength
@@ -92,15 +106,14 @@ app.get('/guest_token', function(req, res){
 		//return the access token object (json)
 		res.send(body);
 	});
-
-
 });
 
 // Refresh token service
 // See API reference - https://spark.autodesk.com/developers/reference/authentication?deeplink=%2Freference%2Foauth-2.0%2Faccess-token-refresh
 app.get('/refresh_token', function(req, res) {
+	console.log("session is:" , req.session);
 	var url = API_SERVER + '/oauth/refreshtoken',
-		params = "grant_type=refresh_token&refresh_token=" + req.query.refresh_token,
+		params = "grant_type=refresh_token&refresh_token=" + req.session.refresh_token,
 		contentLength = params.length,
 		headers = {
 			'Authorization': 'Basic ' + toBase64(config.APP_KEY + ':' + config.APP_SECRET),
@@ -116,7 +129,13 @@ app.get('/refresh_token', function(req, res) {
 		method: 'POST'
 	}, function (err, result, body) {
 		//return the access token object (json)
-		res.send(body);
+		var resp = JSON.parse(body);
+
+		if (resp.refresh_token) {
+			req.session.refresh_token = JSON.parse(JSON.stringify(resp.refresh_token));
+		}
+
+		res.send(resp);
 	});
 });
 
