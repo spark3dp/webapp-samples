3631c91257bdce177b6864c75a769889344c269b
Greg Rashkevitch <rashkevitch@gmail.com>
2015-07-13 13:34:01 +0300

    reverse prev changes

--- assets/scripts/init.js
+++ assets/scripts/init.js
@@ -1,14 +1,17 @@
 //Init the Spark Client - you may supply your personal config through the assets/scripts/config.js file
 var APP_KEY = APP_KEY || '',
-	SERVER_URL = SERVER_URL || 'http://localhost:3000',
+	GUEST_TOKEN_URL = GUEST_TOKEN_URL || 'http://localhost:3000/guest_token',
+	ACCESS_TOKEN_URL = ACCESS_TOKEN_URL || 'http://localhost:3000/access_token',
+	REFRESH_TOKEN_URL = REFRESH_TOKEN_URL || 'http://localhost:3000/refresh_token',
 	REDIRECT_URI = REDIRECT_URI || '';
 
 var isProd = (ENVIRONMENT == "sandbox") ? false : true;
 
-ADSKSpark.Client.initialize(
-	APP_KEY,
+ADSKSpark.Client.initialize(APP_KEY,// Your app key
 	isProd,
 	REDIRECT_URI,
-	SERVER_URL
+	GUEST_TOKEN_URL,// The guest token endpoint that is implemented by your server (i.e. http://example.com/guest_token)
+	ACCESS_TOKEN_URL,// The access token endpoint that is implemented by your server (i.e. http://example.com/access_token)
+	REFRESH_TOKEN_URL// The refresh access token endpoint that is implemented by your server (i.e. http://example.com/refresh_token)
 
 );
